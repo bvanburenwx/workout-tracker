e219fd8e5bfcddc618571823a4bc6cf13c6124b0
BVanBuren <bvanburenwx@gmail.com>
2021-10-15 21:41:05 -0400

    deploying

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(logger("dev"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // routes
 app.use(routes);
